3c2a168165ad20e12b99ccbe1a9c967a4615f87e
Kaolhou <xdroid42@gmail.com>
2020-10-23 16:01:56 -0300

    query mysql e edição links

---
+++ _sql/query.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE `TCM` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+use TCM;
+create table denum(
+	id int primary key auto_increment,
+    nome varchar(40),
+    idade tinyint,
+    genero enum('M', 'F', 'NB', 'PFR') not null,
+    oqhouve text not null
+)CHARACTER SET utf8 COLLATE utf8_general_ci;
